5d4e5076ac55f92924ae312682bda3c5c504078a
Mian Uddin <mianuddin@gmail.com>
2020-03-26 21:22:16 -0700

    predict rating using cf

--- src/reagent_movielens_collaborative_filtering/components/movies.cljs
+++ src/reagent_movielens_collaborative_filtering/components/movies.cljs
@@ -3,6 +3,12 @@
     [reagent.core :as r]
     [dataframe-js :as DataFrame]))
 
+; centers a user's ratings by their mean
+(defn center-ratings [df user-id]
+  (.chain df
+          (fn [row]
+              (.set row "rating" (- (.get row "rating") (.stat.mean (.filter df #js {:userId user-id}) "rating"))))))
+
 ; creates a set of user ids from a given dataframe
 (defn get-df-user-ids [df]
   (-> df
@@ -26,7 +32,7 @@
                                          (Math/sqrt (.reduce df (partial denominator-reduction "gRating") 0)))))
 
 ; calculate how similar the given movie is to the target
-(defn calculate-similarity [target-id given-id movies ratings]
+(defn calculate-similarity [movies ratings target-id given-id]
   (def target-ratings (.filter ratings #js {:movieId target-id}))
   (def given-ratings (.filter ratings #js {:movieId given-id}))
   (def shared-user-ids (clojure.set/intersection (get-df-user-ids target-ratings) (get-df-user-ids given-ratings)))
@@ -38,6 +44,14 @@
                                 (.renameAll #js ["userId", "gMovieId", "gRating"])))
   (-> (.join shared-target-ratings shared-given-ratings "userId")
       cosine-similarity))
+
+; predict rating of a movie using item-based collaborative filtering
+(defn predict-rating [movies ratings target-id user-ratings & [neighborhood-size]]
+  (-> user-ratings
+      (.chain (fn [row] (.set row "similarity" (calculate-similarity movies ratings target-id (.get row "movieId")))))
+      (.sortBy "similarity" true)
+      (.slice 0 (or neighborhood-size 10))
+      (.stat.mean "rating")))
 
 (defn component []
   (let [movies (r/atom nil)
@@ -51,13 +65,30 @@
     (fn []
       (if (and @movies @ratings)
         (let [movies-collection (-> @movies
-                                    (.slice 0 16)
+                                    (.slice 0 5)
                                     .toCollection)]
-          [:div (for [movie movies-collection]
+          [:<> [:p "Let's try to recommend a movie!"]
+               [:p "First, let's see if our movies loaded in correctly."]
+               (for [movie movies-collection]
                   [:div {:key (.-movieId movie)}
                         [:pre {:style {:display "inline" :margin-right "1em"}} (.-movieId movie)]
                         [:p {:style {:display "inline"}} (.-title movie)]])
-                [:pre (.show @movies 10 true)]
-                [:pre (.show @ratings 10 true)]
-                [:p "The cosine similarity between movies 6 and 1 is " (calculate-similarity 6 1 @movies @ratings)]])
+               [:p "If you can see the names of five movies, we're on track. "
+                   "Now, let's take a look at the dataframes. Here are the first ten entries of the movies:"]
+               [:pre (.show @movies 10 true)]
+               [:p "Here are the first ten entries of the ratings:"]
+               [:pre (.show @ratings 10 true)]
+               (let [new-ratings (new js/DataFrame
+                                      #js [#js [0, 1, 3.0], #js [0, 3, 4.0], #js [0, 5, 2.5], #js [0, 7, 1.0]]
+                                      #js ["userId", "movieId", "rating"])]
+                 [:<> [:p "Let's come up with some ratings for a hypothetical user 0:"]
+                      [:pre (.show new-ratings 10 true)]
+                      [:p "Here are the same ratings centered by mean:"]
+                      [:pre (.show (center-ratings new-ratings 0) 10 true)]
+                      [:p "Now, let's try to predict user 0's rating of movie 6."]
+                      [:p "We'll need to calculate cosine similarities between movies. "
+                          "For example, the cosine similarity between movies 6 and 1 is "
+                          (calculate-similarity @movies @ratings 6 1)]
+                      [:p "Here's our predicted rating for movie 6: "
+                          (predict-rating @movies @ratings 6 (center-ratings new-ratings 0) 2)]])])
         [:p "Loading..."]))))
