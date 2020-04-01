3c34c824c3d8befce90739fbd7e18ac429117947
Mian Uddin <mianuddin@gmail.com>
2020-04-01 13:33:43 -0700

    fix indentation in rating

--- src/reagent_movielens_collaborative_filtering/components/rating.cljs
+++ src/reagent_movielens_collaborative_filtering/components/rating.cljs
@@ -1,14 +1,15 @@
 (ns reagent-movielens-collaborative-filtering.components.rating)
 
 (defn component [val]
-  [:div [:input {:min 0.5
-                 :max 5
-                 :on-change #(reset! val (-> % .-target .-value))
-                 :placeholder "e.g. 2.5"
-                 :step 0.5
-                 :style {:display "inline"
-                         :width "5em"}
-                 :type "number"
-                 :value @val}]
-        [:p {:style {:display "inline"}}
-            " / 5"]])
+  [:div
+   [:input {:min 0.5
+            :max 5
+            :on-change #(reset! val (-> % .-target .-value))
+            :placeholder "e.g. 2.5"
+            :step 0.5
+            :style {:display "inline"
+                    :width "5em"}
+            :type "number"
+            :value @val}]
+   [:p {:style {:display "inline"}}
+    " / 5"]])
